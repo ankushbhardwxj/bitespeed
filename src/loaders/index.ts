4f7060531e566d7105f4df706d13726d87553fef
ankushbhardwxj <ankush.bhardwaj0@gmail.com>
2023-08-11 15:12:25 +0530

    chore: added prisma loader, typedi

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import expressLoader from "./express";
+import prismaLoader from "./prisma";
+import Container from "typedi";
 
 export default async (app: express.Application) => {
   expressLoader(app);
+  const prisma = prismaLoader();
+  Container.set("prisma", prisma);
 };
